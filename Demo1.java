ebcc02a73752899841902d31803753834c41873b
geetha3958g <72492138+geetha3958g@users.noreply.github.com>
2020-10-07 13:38:31 +0530

    Add files via upload

---
+++ Demo1.java
@@ -0,0 +1,15 @@
+public class  Demo1
+{
+	int x=10;
+	int y=20;
+	void show(){
+		System.out.println("Value of x:"+x);
+		System.out.println("Value of y:"+y);
+	}
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World!");
+		show();
+		
+	}
+}
